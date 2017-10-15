031917006b0c45523fee04d70e1d29ec5cfcd486
Bastian Kröger <bastian.kroeger@outlook.com>
2017-10-15 14:04:20 +0200

    added node model enabled cors

---
+++ CoreServer/Models/NodeModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreServer.Models
+{
+    public class NodeModel
+    {
+
+        public string Id { get; set; }
+        public string ParentId { get; set; }
+        public string RocketId { get; set; }
+        public string Text { get; set; }
+        public string Flags { get; set; }
+        public double Order { get; set; }
+        public bool ChildrenVisibility { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+
+    }
+}
